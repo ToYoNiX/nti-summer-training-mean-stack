a090ce9545a9b821cdc212c02c8f8a54e1a29087
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-08-10 08:11:40 +0300

    Using a statement function instead.

--- Labs/Lab 5/todos.js
+++ Labs/Lab 5/todos.js
@@ -1,7 +1,7 @@
 const fs = require('node:fs');
 var jsonFile = fs.readFileSync('./tasks.js', 'utf-8');
 
-(function main (args) {
+function main (args) {
     if (args.length < 1) {
         console.log("No argument passed")
         return -1
@@ -25,5 +25,6 @@ var jsonFile = fs.readFileSync('./tasks.js', 'utf-8');
     } else {
         console.log(`Invalid order!!!\nHere is the available orders: \n1. Add {Task Context}\n2. list\n3. Edit {Task ID} {Task Context}\n4. Delete {Task ID}`)
     }
+}
 
-})(process.argv.slice(2))
+main(process.argv.slice(2));
