a6348164a4297a34ba74b0ae77d2f855e983250a
ToYoNiX <toyonix.assemmohamed.2005@gmail.com>
2025-08-10 09:39:10 +0300

    task base is done. Not refined yetgit add *

---
+++ Labs/Lab 5/test.js
@@ -0,0 +1,7 @@
+var obj = [{
+    "content": "this is a task"
+}]
+
+console.log(obj)
+obj.push({"content": "this is the new task"})
+console.log(obj)
--- Labs/Lab 5/todos.js
+++ Labs/Lab 5/todos.js
@@ -1,5 +1,5 @@
 const fs = require('node:fs');
-var jsonFile = fs.readFileSync('./tasks.js', 'utf-8');
+var filePath = "./tasks.json"
 
 function main (args) {
     if (args.length < 1) {
@@ -7,21 +7,44 @@ function main (args) {
         return -1
     }
 
-    // Get the command for the system
     const command = args[0].toLowerCase()    
-    let tasks = eval(jsonFile)
+    let tasks = eval(fs.readFileSync(filePath, 'utf-8'))
     if (command == "add") {
 
+        if (args.length < 2) {
+            console.log("No task statement to add!")
+            return -1
+        }
+
+        tasks.push({"content": args[1]})
+        fs.writeFileSync(filePath, JSON.stringify(tasks))
     } else if (command == "list") {
 
         console.log("ID : Content")
+        let id = 0
         for (let task of tasks) {
-            console.log(task.id, ":", task.content)
+            console.log(id, ":", task.content);
+            id += 1;
         }
     } else if (command == "edit") {
+        
+        if (args.length < 2) {
+            console.log("Invalid arguments!")
+            return -1
+        }
 
+        tasks[args[1]] = {"content":args[2]}
+        fs.writeFileSync(filePath, JSON.stringify(tasks))
     } else if (command == "delete") {
+        
+        
+        if (args.length < 2) {
+            console.log("Invalid arguments!")
+            return -1
+        }
 
+        tasks.splice(args[1], 1)
+        fs.writeFileSync(filePath, JSON.stringify(tasks))
     } else {
         console.log(`Invalid order!!!\nHere is the available orders: \n1. Add {Task Context}\n2. list\n3. Edit {Task ID} {Task Context}\n4. Delete {Task ID}`)
     }
